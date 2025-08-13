3eaf00f2a62a2c62ea69f5b784b400bb0e7e2b19
ning <1241270624@qq.com>
2025-08-13 23:13:43 +0800

    删除错误提交

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/wxgz/WxgzGzztxsController.java
@@ -0,0 +1,101 @@
+package com.ruoyi.web.controller.wxgz;
+
+import cn.hutool.json.JSONUtil;
+import com.ruoyi.common.annotation.Anonymous;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.wxgz.domain.WxgzGzztxs;
+import com.ruoyi.wxgz.service.IWxgzGzztxsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * 工作状态显示Controller
+ *
+ * @author ruoyi
+ * @date 2025-06-14
+ */
+@RestController
+@RequestMapping("/wxgz/gzztxs")
+@Anonymous
+public class WxgzGzztxsController extends BaseController {
+    @Autowired
+    private IWxgzGzztxsService wxgzGzztxsService;
+
+    /**
+     * HTML->Java
+     * 查询工作状态显示列表
+     */
+    @GetMapping("/list")
+    public TableDataInfo list(WxgzGzztxs wxgzGzztxs) {
+        logger.info("前端状态显示请求：{}", JSONUtil.toJsonStr(wxgzGzztxs));
+        /*if (wxgzGzztxs.getParams() == null || wxgzGzztxs.getParams().isEmpty()) {
+            logger.info("不分页");
+            return getDataTable(wxgzGzztxsService.selectWxgzGzztxsList(wxgzGzztxs));
+        }*/
+        startPage();
+        logger.info("分页");
+        List<WxgzGzztxs> list = wxgzGzztxsService.selectWxgzGzztxsList(wxgzGzztxs);
+        return getDataTable(list);
+    }
+
+    /**
+     * C->Java
+     * 新增工作状态显示
+     */
+    @Log(title = "工作状态显示", businessType = BusinessType.INSERT)
+    @PostMapping
+    public AjaxResult add(@RequestBody String param) {
+        logger.info("工作状态显示进来了:{}", param);
+        return toAjax(wxgzGzztxsService.insertWxgzGzztxs(new WxgzGzztxs() {{
+            setContent(param);
+        }}));
+    }
+
+    /**
+     * 获取工作状态显示详细信息
+     */
+    @GetMapping(value = "/orderByIdDesc")
+    public AjaxResult getInfo() {
+        return success(wxgzGzztxsService.selectWxgzGzztxsOrderByIdDesc());
+    }
+
+    /**
+     * 导出工作状态显示列表
+     */
+    @Log(title = "工作状态显示", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, WxgzGzztxs wxgzGzztxs) {
+        List<WxgzGzztxs> list = wxgzGzztxsService.selectWxgzGzztxsList(wxgzGzztxs);
+        ExcelUtil<WxgzGzztxs> util = new ExcelUtil<WxgzGzztxs>(WxgzGzztxs.class);
+        util.exportExcel(response, list, "工作状态显示数据");
+    }
+
+    /**
+     * 修改工作状态显示
+     */
+    @PreAuthorize("@ss.hasPermi('wxgz:gzztxs:edit')")
+    @Log(title = "工作状态显示", businessType = BusinessType.UPDATE)
+    @PutMapping
+    public AjaxResult edit(@RequestBody WxgzGzztxs wxgzGzztxs) {
+        return toAjax(wxgzGzztxsService.updateWxgzGzztxs(wxgzGzztxs));
+    }
+
+    /**
+     * 删除工作状态显示
+     */
+    @PreAuthorize("@ss.hasPermi('wxgz:gzztxs:remove')")
+    @Log(title = "工作状态显示", businessType = BusinessType.DELETE)
+    @DeleteMapping("/{ids}")
+    public AjaxResult remove(@PathVariable Long[] ids) {
+        return toAjax(wxgzGzztxsService.deleteWxgzGzztxsByIds(ids));
+    }
+}
